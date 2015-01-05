22b992227fbdd23afe50b43976ef83f6a37fd1b4
Jennifer Scroggins <jennifer@ravenwilde.com>
2015-01-04 22:47:57 -0500

    navigation styles and page scroll implemented - navigation links working - removed drop shadow from header img

--- js/forma.js
+++ js/forma.js
@@ -23,3 +23,30 @@ $(document).ready(function() {
     $(this).toggleClass("expander-hidden");
   });
 });
+
+(function (jQuery) {
+  jQuery.mark = {
+    jump: function (options) {
+      var defaults = {
+        selector: 'a.scroll-on-page-link'
+      };
+      if (typeof options == 'string') defaults.selector = options;
+      var options = jQuery.extend(defaults, options);
+      return jQuery(options.selector).click(function (e) {
+        var jumpobj = jQuery(this);
+        var target = jumpobj.attr('href');
+        var thespeed = 600;
+        var offset = jQuery(target).offset().top;
+        jQuery('html,body').animate({
+          scrollTop: offset
+        }, thespeed, 'swing')
+        e.preventDefault();
+      })
+    }
+  }
+})(jQuery);
+
+
+jQuery(function(){  
+  jQuery.mark.jump();
+});
